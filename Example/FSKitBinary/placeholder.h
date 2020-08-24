3da1f3a624527b3ba2d62ccf311b5c3f72e02837
fengshuo_2 <963787902@qq.com>
2020-08-24 23:30:25 +0800

    修改了tag

---
+++ Example/FSKitBinary/placeholder.h
@@ -0,0 +1,13 @@
+//
+//  FSKitBinary.h
+//  FSKitBinary
+//
+//  Created by fengshuo on 2020/8/24.
+//  Copyright © 2020 fengshuo1992. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface placeholder : NSObject
+
+@end
