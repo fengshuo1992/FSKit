a4d8464c23139866856eb572d226ce5d18da47f4
fengshuo_2 <963787902@qq.com>
2020-08-24 23:55:07 +0800

    aaa

---
+++ FSKit/Classes/TestAPiObject.h
@@ -0,0 +1,19 @@
+//
+//  TestAPiObject.h
+//  XCChatView
+//
+//  Created by fengshuo on 2020/8/24.
+//  Copyright © 2020 fengshuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestAPiObject : NSObject
+
+- (void)name;
+
+@end
+
+NS_ASSUME_NONNULL_END
